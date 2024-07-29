f09586374fd70bc15714c328e3af735ed15009ef
Teru <143543521+ErutTheTeru@users.noreply.github.com>
2024-07-29 18:05:13 +0700

    Update Source.lua

--- Setting Func/Source.lua
+++ Setting Func/Source.lua
@@ -22,18 +22,20 @@ end
 getgenv().loadsetting = function(foldername, filename, tabs)
     local UIConfig = readdata(foldername, filename, tabs)
     for Tab, TabFunc in tabs do
-        for NameItem, Item in TabFunc do
-            if type(Item) == "table" then
-                if Item.Type == "Dropdown" then
-                    Item:Refresh(UIConfig[Tab][NameItem].Options, UIConfig[Tab][NameItem].Value)
-                else
-                    if v.Type ~= "Button" and UIConfig[Tab][NameItem] and UIConfig[Tab][NameItem].Value ~= Item.Value then
-                        Item:Set(UIConfig[Tab][NameItem].Value)
-                    end
-                    if Item["Setting Item"] then
-                        for _, v in Item["Setting Item"] do
-                            if UIConfig[Tab][NameItem]["Setting Item"] and UIConfig[Tab][NameItem]["Setting Item"] ~= v.Value then
-                                v:Set(UIConfig[Tab][NameItem]["Setting Item"].Value)
+        if UIConfig[Tab] then
+            for NameItem, Item in TabFunc do
+                if type(Item) == "table" and UIConfig[Tab][NameItem] then
+                    if Item.Type == "Dropdown" then
+                        Item:Refresh(UIConfig[Tab][NameItem].Option, UIConfig[Tab][NameItem].Value)
+                    else
+                        if v.Type ~= "Button" and UIConfig[Tab][NameItem] and UIConfig[Tab][NameItem].Value ~= Item.Value then
+                            Item:Set(UIConfig[Tab][NameItem].Value)
+                        end
+                        if Item["Setting Item"] then
+                            for _, v in Item["Setting Item"] do
+                                if UIConfig[Tab][NameItem]["Setting Item"] and UIConfig[Tab][NameItem]["Setting Item"] ~= v.Value then
+                                    v:Set(UIConfig[Tab][NameItem]["Setting Item"].Value)
+                                end
                             end
                         end
                     end
