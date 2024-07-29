c4f878dec042f9b42298643b07b79d0cb3a1b93d
Teru <143543521+ErutTheTeru@users.noreply.github.com>
2024-07-29 16:34:04 +0700

    Update Source.lua

--- Setting Func/Source.lua
+++ Setting Func/Source.lua
@@ -1,5 +1,4 @@
 getgenv().save = function(foldername, filename, filecontent)
-    print(unpack(filecontent))
     local filename = foldername.."/"..filename..".json"
     local filecontent = game:GetService("HttpService"):JSONEncode(filecontent)
     if isfolder(foldername) then
@@ -24,21 +23,23 @@ getgenv().loadsetting = function(foldername, filename, tabs)
     local UIConfig = readdata(foldername, filename, tabs)
     for Tab, TabFunc in tabs do
         for NameItem, Item in TabFunc do
-            if Item.Type == "Dropdown" then
-                for _, v in Item.Value do
-                    if not Win11Config[Tab][NameItem] or not Win11Config[Tab][NameItem].Value[v] then
-                        Item:Set(Win11Config[Tab][NameItem].Value)
-                        break
+            if Item.Type then
+                if Item.Type == "Dropdown" then
+                    for _, v in Item.Value do
+                        if not Win11Config[Tab][NameItem] or not Win11Config[Tab][NameItem].Value[v] then
+                            Item:Set(Win11Config[Tab][NameItem].Value)
+                            break
+                        end
                     end
-                end
-            else
-                if v.Type ~= "Button" and Win11Config[Tab][NameItem] and Win11Config[Tab][NameItem].Value ~= Item.Value then
-                    Item:Set(Win11Config[Tab][NameItem].Value)
-                end
-                if Item["Setting Item"] then
-                    for _, v in Item["Setting Item"] do
-                        if Win11Config[Tab][NameItem]["Setting Item"] and Win11Config[Tab][NameItem]["Setting Item"] ~= v.Value then
-                            v:Set(Win11Config[Tab][NameItem]["Setting Item"].Value)
+                else
+                    if v.Type ~= "Button" and Win11Config[Tab][NameItem] and Win11Config[Tab][NameItem].Value ~= Item.Value then
+                        Item:Set(Win11Config[Tab][NameItem].Value)
+                    end
+                    if Item["Setting Item"] then
+                        for _, v in Item["Setting Item"] do
+                            if Win11Config[Tab][NameItem]["Setting Item"] and Win11Config[Tab][NameItem]["Setting Item"] ~= v.Value then
+                                v:Set(Win11Config[Tab][NameItem]["Setting Item"].Value)
+                            end
                         end
                     end
                 end
