c936e7999158c3ec801df8ab5b2168cd4ca3f5b2
Teru <143543521+ErutTheTeru@users.noreply.github.com>
2024-07-30 12:39:25 +0700

    Update Source.lua

--- Setting Func/Source.lua
+++ Setting Func/Source.lua
@@ -21,22 +21,22 @@ end
 
 getgenv().loadsetting = function(foldername, filename, tabs)
     local UIConfig = readdata(foldername, filename, tabs)
-    for Tab, TabFunc in tabs do
-        if UIConfig[Tab] then
+    for NameTab, TabFunc in tabs do
+        if UIConfig[NameTab] then
             for NameItem, Item in TabFunc do
-                if type(Item) == "table" and UIConfig[Tab][NameItem] and Item.Type and UIConfig[Tab][NameItem].Type then
+                if type(Item) == "table" and UIConfig[NameTab][NameItem] and Item.Type and UIConfig[NameTab][NameItem].Type then
                     if Item.Type == "Dropdown" then
-                        Item:Refresh(UIConfig[Tab][NameItem].Options, UIConfig[Tab][NameItem].Value)
+                        Item:Refresh(UIConfig[NameTab][NameItem].Options, UIConfig[NameTab][NameItem].Value)
                     else
-                        if Item.Type ~= "Button" and UIConfig[Tab][NameItem] and UIConfig[Tab][NameItem].Value ~= Item.Value then
-                            Item:Set(UIConfig[Tab][NameItem].Value)
+                        if Item.Type ~= "Button" and UIConfig[NameTab][NameItem] and UIConfig[NameTab][NameItem].Value ~= Item.Value then
+                            Item:Set(UIConfig[TNameab][NameItem].Value)
                         end
-                        if Item["Setting Item"] then
-                            for i, v in Item["Setting Item"] do
-                                if UIConfig[Tab][NameItem]["Setting Item"][i] and UIConfig[Tab][NameItem]["Setting Item"][i] ~= v.Value then
-                                    v:Set(UIConfig[Tab][NameItem]["Setting Item"][i].Value)
-                                end
-                            end
+                    end
+                end
+                if Item["Setting Item"] then
+                    for i, v in Item["Setting Item"] do
+                        if UIConfig[NameTab][NameItem]["Setting Item"][i] and UIConfig[NameTab][NameItem]["Setting Item"][i] ~= v.Value then
+                            v:Set(UIConfig[NameTab][NameItem]["Setting Item"][i].Value)
                         end
                     end
                 end
