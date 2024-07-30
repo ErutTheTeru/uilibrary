6df85202a76f7396517cd5a44a0e42b5bc2810d9
Teru <143543521+ErutTheTeru@users.noreply.github.com>
2024-07-30 09:57:36 +0700

    Update Source.lua

--- Setting Func/Source.lua
+++ Setting Func/Source.lua
@@ -28,7 +28,7 @@ getgenv().loadsetting = function(foldername, filename, tabs)
                     if Item.Type == "Dropdown" then
                         Item:Refresh(UIConfig[Tab][NameItem].Option, UIConfig[Tab][NameItem].Value)
                     else
-                        if v.Type ~= "Button" and UIConfig[Tab][NameItem] and UIConfig[Tab][NameItem].Value ~= Item.Value then
+                        if Item.Type ~= "Button" and UIConfig[Tab][NameItem] and UIConfig[Tab][NameItem].Value ~= Item.Value then
                             Item:Set(UIConfig[Tab][NameItem].Value)
                         end
                         if Item["Setting Item"] then
